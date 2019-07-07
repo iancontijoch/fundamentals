a29a1a5dbc7ca89a138ecce3ed461b0f10858b11
Ian Contijoch <ian.contijoch@gmail.com>
2019-07-07 09:37:35 -0400

    minor run.py adjustments

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from sorts import bubble_sort, selection_sort
+from sorts import bubble_sort, selection_sort, quicksort
 
 l = [5, 3, 4, 2, 1]
 
-print(bubble_sort(l))
-print(selection_sort(l))
+# print(bubble_sort(l))
+# print(selection_sort(l))
+quicksort(l)
